bf9d8d1c9f5f3f0cf03ba4d7cc7cbe6dfccc7f1d
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-11-26 15:43:22 -0500

    Update vNAS Constants

--- modules/vNAS.py
+++ modules/vNAS.py
@@ -1,1 +1,31 @@
+ASDEX_STYLES = ["runway", "taxiway", "apron", "structure"]
+BCG_MIN = 1
+BCG_MAX = 40
+FILTER_MIN = 0
+FILTER_MAX = 40
 LINE_STYLES = ["solid", "longDashed", "shortDashed", "longDashShortDash"]
+LINE_THICKNESS_MIN = 1
+LINE_THICKNESS_MAX = 3
+SYMBOL_STYLES = [
+    "obstruction1",
+    "obstruction2",
+    "heliport",
+    "nuclear",
+    "emergencyAirport",
+    "radar",
+    "iaf",
+    "rnavOnlyWaypoint",
+    "rnav",
+    "airwayIntersections",
+    "ndb",
+    "vor",
+    "otherWaypoints",
+    "airport",
+    "satelliteAirport",
+    "tacan",
+    "dme",
+]
+SYMBOL_SIZE_MIN = 1
+SYMBOL_SIZE_MAX = 4
+TEXT_SIZE_MIN = 0
+TEXT_SIZE_MAX = 5
