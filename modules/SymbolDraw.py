f8724a7ddb70de85dd96b0b239fe06f437313156
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-11-27 17:40:09 -0500

    Init SymbolDraw Module

---
+++ modules/SymbolDraw.py
@@ -0,0 +1,47 @@
+from modules.DrawHelper import correctOffsets
+from modules.GeoJSON import CoordinatePair, Feature, LineString, MultiLineString
+from modules.SymbolPlots import PLOT_HEIGHT, PLOT_WIDTH, UNRECOGNIZED, getPlotFromString
+
+
+class SymbolDraw:
+    def __init__(
+        self,
+        symbolType: str,
+        lat: float,
+        lon: float,
+        rotationDeg: float = 0.0,
+        symbolScale: float = 1.0,
+    ):
+        self.symbolType = symbolType
+        self.lat = lat
+        self.lon = lon
+        self.rotationDeg = rotationDeg
+        self.symbolScale = symbolScale
+        self.feature = Feature()
+
+        self._toPointArray()
+
+    def _toPointArray(self) -> None:
+        multiLineString = MultiLineString()
+        lineString = LineString()
+        symbolPlot = getPlotFromString(self.symbolType)
+        if symbolPlot != UNRECOGNIZED:
+            correctedPlot = correctOffsets(
+                self.lat,
+                self.lon,
+                symbolPlot,
+                PLOT_HEIGHT,
+                PLOT_WIDTH,
+                self.rotationDeg,
+                self.symbolScale,
+            )
+
+            for line in correctedPlot:
+                coordinatePair = CoordinatePair(line["lat"], line["lon"])
+                lineString.addCoordinatePair(coordinatePair)
+            if not lineString.isEmpty():
+                multiLineString.addLineString(lineString)
+            self.feature.addMultiLineString(multiLineString)
+
+    def getFeature(self) -> Feature:
+        return self.feature
