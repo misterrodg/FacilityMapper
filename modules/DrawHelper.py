7209955798b4363c66fb5d3f669eb5067b6437c1
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-11-27 22:54:39 -0500

    Add DrawHelper Functions

--- modules/DrawHelper.py
+++ modules/DrawHelper.py
@@ -1,6 +1,8 @@
 import math
 
 ARC_MIN = 0.015  # 1 min of arc = 1/60, most facilities round this down to 0.015 resulting in 0.9nm tall letters
+DEG_TO_MIN = 60
+EARTH_RADIUS_NM = 3443.92
 
 
 def correctOffsets(
@@ -35,3 +37,58 @@ def correctOffsets(
         result.append({"lat": adjustedLat, "lon": adjustedLon})
 
     return result
+
+
+def latLonFromPBD(lat: float, lon: float, bearing: float, distance: float) -> dict:
+    lat = math.radians(lat)
+    lon = math.radians(lon)
+    bearing = math.radians(bearing)
+    endLat = math.asin(
+        math.sin(lat) * math.cos(distance / EARTH_RADIUS_NM)
+        + math.cos(lat) * math.sin(distance / EARTH_RADIUS_NM) * math.cos(bearing)
+    )
+    endLon = lon + math.atan2(
+        math.sin(bearing) * math.sin(distance / EARTH_RADIUS_NM) * math.cos(lat),
+        math.cos(distance / EARTH_RADIUS_NM) - math.sin(lat) * math.sin(endLat),
+    )
+
+    endLat = math.degrees(endLat)
+    endLon = math.degrees(endLon)
+
+    result = {"lat": endLat, "lon": endLon}
+    return result
+
+
+def haversineGreatCircleDistance(
+    startLat: float, startLon: float, endLat: float, endLon: float
+) -> float:
+    theta = startLon - endLon
+    arc = math.degrees(
+        math.acos(
+            (math.sin(math.radians(startLat)) * math.sin(math.radians(endLat)))
+            + (
+                math.cos(math.radians(startLat))
+                * math.cos(math.radians(endLat))
+                * math.cos(math.radians(theta))
+            )
+        )
+    )
+    distance = arc * DEG_TO_MIN
+    return distance
+
+
+def inverseBearing(bearing: float) -> float:
+    result = math.fmod(bearing + 180, 360)
+    return result
+
+
+def haversineGreatCircleBearing(
+    startLat: float, startLon: float, endLat: float, endLon: float
+) -> float:
+    x = math.cos(math.radians(startLat)) * math.sin(math.radians(endLat)) - math.sin(
+        math.radians(startLat)
+    ) * math.cos(math.radians(endLat)) * math.cos(math.radians(endLon - startLon))
+    y = math.sin(math.radians(endLon - startLon)) * math.cos(math.radians(endLat))
+    bearing = math.degrees(math.atan2(y, x))
+    bearing = math.fmod(bearing + 360, 360)
+    return bearing
