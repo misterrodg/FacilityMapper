e52a8c284b139f2104cdb687955d7de5135daa40
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-12-04 12:42:09 -0500

    Add vNAS Property Types

--- modules/vNAS.py
+++ modules/vNAS.py
@@ -6,6 +6,16 @@ FILTER_MAX = 40
 LINE_STYLES = ["solid", "longDashed", "shortDashed", "longDashShortDash"]
 LINE_THICKNESS_MIN = 1
 LINE_THICKNESS_MAX = 3
+PROPERTY_TYPE_LINE = "line"
+PROPERTY_TYPE_POINT = "point"
+PROPERTY_TYPE_POLYGON = "polygon"
+PROPERTY_TYPE_SYMBOL = "symbol"
+PROPERTY_TYPES = [
+    PROPERTY_TYPE_LINE,
+    PROPERTY_TYPE_POINT,
+    PROPERTY_TYPE_POLYGON,
+    PROPERTY_TYPE_SYMBOL,
+]
 SYMBOL_STYLES = [
     "obstruction1",
     "obstruction2",
