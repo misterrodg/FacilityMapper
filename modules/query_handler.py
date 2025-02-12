2632b92ac61f09eedb6bff8e05a1261f2d46042a
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-02-12 12:06:35 -0500

    Add query_db_one Function

--- modules/query_handler.py
+++ modules/query_handler.py
@@ -4,3 +4,9 @@ from sqlite3 import Cursor
 def query_db(db_cursor: Cursor, query_string: str) -> list[dict]:
     db_cursor.execute(query_string)
     return [dict(row) for row in db_cursor.fetchall()]
+
+
+def query_db_one(db_cursor: Cursor, query_string: str) -> dict:
+    db_cursor.execute(query_string)
+    row = db_cursor.fetchone()
+    return dict(row) if row else None
