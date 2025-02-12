99c48734b9cf8cc9f5066715513423aef81c1564
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-02-12 11:23:47 -0500

    Implement ProcedureRecord and ProcedureRecords

--- modules/db/__init__.py
+++ modules/db/__init__.py
@@ -1,5 +1,11 @@
 from .controlled_record import ControlledRecord
 from .controlled_records import ControlledRecords, select_controlled_points
+from .procedure_record import JoinedProcedureRecord, ProcedureRecord
+from .procedure_records import (
+    JoinedProcedureRecords,
+    ProcedureRecords,
+    select_joined_procedure_points,
+)
 from .restrictive_record import RestrictiveRecord
 from .restrictive_records import RestrictiveRecords, select_restrictive_points
 from .runway_record import RunwayRecord
@@ -9,6 +15,11 @@ __all__ = [
     "ControlledRecord",
     "ControlledRecords",
     "select_controlled_points",
+    "JoinedProcedureRecord",
+    "JoinedProcedureRecords",
+    "ProcedureRecord",
+    "ProcedureRecords",
+    "select_joined_procedure_points",
     "RestrictiveRecord",
     "RestrictiveRecords",
     "select_restrictive_points",
