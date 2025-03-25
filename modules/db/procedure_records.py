8f60d5517e79902c2447eebc713fe73033d1e9f6
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-03-25 15:58:55 -0400

    Overhaul Procedure Records Query

--- modules/db/procedure_records.py
+++ modules/db/procedure_records.py
@@ -1,14 +1,41 @@
 from modules.db.procedure_record import ProcedureRecord, JoinedProcedureRecord
+from modules.db.query_helper import (
+    list_to_sql_string,
+    str_to_sql_string,
+    translate_condition,
+)
+from modules.db.record_helper import segment_records
+
+
+def _handle_transitions(transition_ids: list) -> str:
+    if "ALL" in transition_ids:
+        return ""
+    if transition_ids:
+        transition_ids_as_string = list_to_sql_string(transition_ids)
+        return f"AND (transition_id IS NULL OR transition_id IN {transition_ids_as_string})"
+    return "AND transition_id IS NULL"
+
+
+def _handle_path_term(path_terms: list) -> str:
+    result = "p.path_term NOT IN ('FM','HA','HF','HM','PI','VM')"
+    if path_terms:
+        path_terms_as_string = list_to_sql_string(path_terms)
+        result = f"p.path_term NOT IN {path_terms_as_string}"
+    return result
 
 
 def select_joined_procedure_points(
-    fac_id: str, fac_sub_code: str, procedure_id: str, transition_id: str = "NULL"
+    fac_id: str,
+    fac_sub_code: str,
+    procedure_id: str,
+    transitions: list = [],
+    path_terms: list = [],
 ) -> str:
-    transition_string = "transition_id IS NULL"
-    if transition_id != "NULL":
-        transition_string = (
-            f"(transition_id IS NULL OR transition_id = {transition_id})"
-        )
+    fac_id_string = str_to_sql_string(fac_id)
+    fac_sub_code_string = str_to_sql_string(fac_sub_code)
+    transition_string = _handle_transitions(transitions)
+    procedure_id_string = translate_condition("procedure_id", procedure_id)
+    path_term_string = _handle_path_term(path_terms)
     result = f"""
     WITH unified_table AS (
         SELECT waypoint_id AS id,lat,lon,type FROM waypoints
@@ -21,13 +48,13 @@ def select_joined_procedure_points(
         UNION
         SELECT ndb_id AS id,lat,lon,"NDB" AS type FROM ndbs
         UNION
-        SELECT runway_id AS id,lat,lon,"RUNWAY" AS type FROM runways WHERE airport_id = {fac_id}
+        SELECT runway_id AS id,lat,lon,"RUNWAY" AS type FROM runways WHERE airport_id = {fac_id_string}
     )
     SELECT p.*,id,lat,lon,type
     FROM procedure_points AS p
     LEFT JOIN unified_table AS u ON p.fix_id = u.id
-    WHERE fac_id = {fac_id} AND fac_sub_code = {fac_sub_code} AND procedure_id = {procedure_id} AND {transition_string} AND p.path_term NOT IN ('FM','HA','HF','HM','PI','VM')
-    ORDER BY p.procedure_id,p.transition_id,p.route_type,p.sequence_number;
+    WHERE fac_id = {fac_id_string} AND fac_sub_code = {fac_sub_code_string} AND {procedure_id_string} {transition_string} AND {path_term_string}
+    ORDER BY p.procedure_id,p.route_type,p.transition_id DESC,p.sequence_number;
     """
     return result
 
