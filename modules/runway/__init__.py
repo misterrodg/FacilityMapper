fbdacf8ac81137107eb08cdc4575898353b30254
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-03-27 14:26:32 -0400

    Add split_runway_id Function

--- modules/runway/__init__.py
+++ modules/runway/__init__.py
@@ -1,6 +1,12 @@
 from .runway_handler import get_line_strings
-from .runway_helper import inverse_runway
+from .runway_helper import inverse_runway, split_runway_id
 from .runway_pair import RunwayPair
 from .runway_pairs import RunwayPairs
 
-__all__ = ["get_line_strings", "inverse_runway", "RunwayPair", "RunwayPairs"]
+__all__ = [
+    "get_line_strings",
+    "inverse_runway",
+    "split_runway_id",
+    "RunwayPair",
+    "RunwayPairs",
+]
--- modules/runway/runway_helper.py
+++ modules/runway/runway_helper.py
@@ -1,13 +1,25 @@
+RUNWAY_PREFIX = "RW"
+
+
 def inverse_runway(runway_id: str) -> str:
-    runway_prefix = "RW"
     bearing_component = int(runway_id[2:4])
     inverse_bearing = _handle_bearing_component(bearing_component)
     side_component = runway_id[4:]
     inverse_side = ""
     if side_component:
         inverse_side = _handle_side_component(side_component)
-    result = f"{runway_prefix}{inverse_bearing:02}{inverse_side}"
+    result = f"{RUNWAY_PREFIX}{inverse_bearing:02}{inverse_side}"
     return result
+
+
+def split_runway_id(runway_id: str) -> dict:
+    bearing_component = runway_id[2:4]
+    side_component = runway_id[4:]
+    return {
+        "prefix": RUNWAY_PREFIX,
+        "bearing_component": bearing_component,
+        "side_component": side_component,
+    }
 
 
 def _handle_bearing_component(bearing_component: int) -> int:
