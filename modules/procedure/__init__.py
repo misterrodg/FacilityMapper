87ef97550681dfbd2d27459a5210e3cc04e1fe45
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-04-03 13:43:34 -0400

    Add Core Route Types

--- modules/procedure/__init__.py
+++ modules/procedure/__init__.py
@@ -1,11 +1,13 @@
 from .procedure_helper import (
     DE_LEADING_ROUTE_TYPES,
+    DE_CORE_ROUTE_TYPES,
     DE_TRAILING_ROUTE_TYPES,
     translate_map_type,
 )
 
 __all__ = [
     "DE_LEADING_ROUTE_TYPES",
+    "DE_CORE_ROUTE_TYPES",
     "DE_TRAILING_ROUTE_TYPES",
     "translate_map_type",
 ]
