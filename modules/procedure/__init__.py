642be6be15a70dea2833b965b9ffe60ce670dfb2
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-04-04 17:13:56 -0400

    Update Route Types

--- modules/procedure/__init__.py
+++ modules/procedure/__init__.py
@@ -3,6 +3,7 @@ from .procedure_helper import (
     DE_LEADING_ROUTE_TYPES,
     DE_CORE_ROUTE_TYPES,
     DE_TRAILING_ROUTE_TYPES,
+    F_LEADING_ROUTE_TYPES,
     translate_map_type,
 )
 
@@ -10,6 +11,7 @@ __all__ = [
     "DE_LEADING_ROUTE_TYPES",
     "DE_CORE_ROUTE_TYPES",
     "DE_TRAILING_ROUTE_TYPES",
+    "F_LEADING_ROUTE_TYPES",
     "get_line_feature",
     "get_symbol_features",
     "get_text_features",
--- modules/procedure/procedure_helper.py
+++ modules/procedure/procedure_helper.py
@@ -1,6 +1,7 @@
 DE_LEADING_ROUTE_TYPES = ["1", "4"]
 DE_CORE_ROUTE_TYPES = ["2", "5"]
 DE_TRAILING_ROUTE_TYPES = ["3", "6"]
+F_LEADING_ROUTE_TYPES = ["A"]
 
 
 def translate_map_type(map_type: str) -> str:
